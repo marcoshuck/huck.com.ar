f4697b670d84e2c8e1a12cc8eb051661af27d787
Marcos Huck <marcos@huck.com.ar>
2023-10-21 20:29:44 -0300

    Add CourseCard

---
+++ src/components/CourseCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  interface Props {
+    title: string;
+    description: string;
+    price: number;
+    currency: string;
+  }
+
+  defineProps<Props>()
+</script>
+
+<template>
+  <v-card>
+    <v-card-title class="text-wrap text-center text-secondary">
+      {{title}}
+    </v-card-title>
+    <v-card-text class="text-body-1">
+      <p class="my-1">{{description}}</p>
+      <p class="my-2 text-center text-secondary font-weight-bold">{{price}} {{currency.toUpperCase()}} / session</p>
+    </v-card-text>
+    <v-card-actions class="align-center justify-center">
+      <v-btn color="primary" size="large" variant="flat" rounded="xs">Schedule</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,11 +1,46 @@
 <script setup lang="ts">
 
+import CourseCard from "@/components/CourseCard.vue";
 </script>
 
 <template>
   <v-container>
     <h1 class="text-h1 text-primary">Services</h1>
-    <h2 class="text-h2">For people</h2>
+    <h2 class="text-h2">For individuals</h2>
+    <v-row class="my-2">
+      <v-col cols="3">
+        <CourseCard
+          title="Test-driven development (TDD)"
+          :price="30"
+          description="Learn and implement Test-driven development in your every day job, simplify your workflow and make great impact in your technical skills by designing code that actually works and solves the problem."
+          currency="usd"
+        ></CourseCard>
+      </v-col>
+      <v-col cols="3">
+        <CourseCard
+          title="The Go programming language"
+          :price="30"
+          description="Learn the perks of using one the best well-known programming languages for distributed systems and cloud applications."
+          currency="usd"
+        ></CourseCard>
+      </v-col>
+      <v-col cols="3">
+        <CourseCard
+          title="Software architecture"
+          :price="50"
+          description="Learn how to design highly scalable systems by analyzing the business, designing domain rules and summarizing all the information in diagrams."
+          currency="usd"
+        ></CourseCard>
+      </v-col>
+      <v-col cols="3">
+        <CourseCard
+          title="Mentorship"
+          :price="30"
+          description="Learn and implement Test-driven development in your every day job, simplify your workflow and make great impact in your technical skills by designing code that actually works and solves the problem."
+          currency="usd"
+        ></CourseCard>
+      </v-col>
+    </v-row>
     <h2 class="text-h2">For businesses</h2>
   </v-container>
 </template>
