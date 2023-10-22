cf13ae8240a51dd90b9c447dc7930b460db9f1d1
Marcos Huck <marcos@huck.com.ar>
2023-10-22 02:12:13 -0300

    Implement Google Analytics in Vue project

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -10,9 +10,20 @@ import router from '../router'
 
 // Types
 import type { App } from 'vue'
+import gtag from 'vue-gtag'
 
 export function registerPlugins (app: App) {
   app
     .use(vuetify)
     .use(router)
+    .use(gtag, {
+      appName: 'Marcos Huck - Website',
+      pageTrackerScreenviewEnabled: true,
+      config: {
+        id: 'UA-43481624-1',
+        params: {
+          anonymize_ip: true,
+        }
+      }
+    }, router)
 }
